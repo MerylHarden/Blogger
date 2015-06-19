156c91c0888f4529d9df05780b1ec77b67a0d361
MerylHarden <merylharden@gmail.com>
2015-06-19 06:13:51 -0700

    finished unit 11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 
 def index
   @articles = Article.all
@@ -6,7 +7,37 @@ end
 
 def show
 	@article = Article.find(params[:id])
-
 end
+
+def new
+	@article = Article.new
+end
+
+ def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+ end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(:title, :body)
+end
+
+def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+
+  flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+end
+
+
+
 
 end
